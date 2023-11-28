c655d56eaf4c9e59d92f183f889da89b584aeda5
Jesus <zticma114@gmail.com>
2023-11-27 18:01:22 -0600

    controllers completos y sevice

--- lib/src/controller/controller_list.dart
+++ lib/src/controller/controller_list.dart
@@ -4,31 +4,31 @@ import 'package:get/get.dart';
 
 class ControllerList extends GetxController {
   var listaEstadio = <EstadioModelo>[].obs;
-  ProductsService service = ProductsService();
+  StadiumsService service = StadiumsService();
 
   @override
   void refresh() {
-    var list = service.loadProducts();
+    var list = service.loadEstadio();
     list.then((value) => listaEstadio.value = value);
   }
 
   void actualizar(EstadioModelo estadio) async {
-    await service.updateProduct(estadio);
+    await service.updateestadio(estadio);
     int index =
-        listaEstadio.value.indexWhere((element) => element.id == producto.id);
+        listaEstadio.value.indexWhere((element) => element.id == estadio.id);
     listaEstadio[index] = estadio;
   }
 
   void delete(EstadioModelo value) {
-    service.deleteProduct(value);
+    service.deleteestadio(value);
     int index =
         listaEstadio.value.indexWhere((element) => element.id == value.id);
     listaEstadio.removeAt(index);
   }
 
   Future<String?> agregar(EstadioModelo estadio) async {
-    String? id = await service.createProduct(estadio);
-    producto.id = id;
+    String? id = await service.createestadio(estadio);
+    estadio.id = id;
     listaEstadio.add(estadio);
     return id;
   }
