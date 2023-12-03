97ebf719f5bf2894fc3760c7fea6f6e7066acf2b
Felix <felix5067456@gmail.com>
2023-12-03 12:25:38 -0600

    amlo

--- lib/src/controller/controller_list_jugador.dart
+++ lib/src/controller/controller_list_jugador.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_application_1/src/models/jugador_modelo.dart';
 import 'package:flutter_application_1/src/service/jugador_service.dart';
 import 'package:get/get.dart';
-// import 'package:worldps/src/models/jugador_modelo.dart';
-// import 'package:worldps/src/service/jugador_service.dart';
 
 class ControllerListJugador extends GetxController {
   var listaJugador = <JugadorModelo>[].obs;
@@ -11,27 +9,32 @@ class ControllerListJugador extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    var list = service.loadPlayers();
-    list.then((value) => listaJugador.value = value);
+    loadPlayers(); // Llama a la función para cargar los jugadores
   }
 
   @override
   void refresh() {
-    var list = service.loadPlayers();
-    list.then((value) => listaJugador.value = value);
+    loadPlayers(); // Llama a la función para recargar los jugadores
+  }
+
+  Future<void> loadPlayers() async {
+    try {
+      var players = await service.loadPlayers();
+      listaJugador.assignAll(players);
+    } catch (e) {
+      print('Error al cargar jugadores: $e');
+    }
   }
 
   void actualizar(JugadorModelo jugador) async {
     await service.updatePlayer(jugador);
-    int index =
-        listaJugador.value.indexWhere((element) => element.id == jugador.id);
+    int index = listaJugador.indexWhere((element) => element.id == jugador.id);
     listaJugador[index] = jugador;
   }
 
   void delete(JugadorModelo value) {
     service.deletePlayer(value);
-    int index =
-        listaJugador.value.indexWhere((element) => element.id == value.id);
+    int index = listaJugador.indexWhere((element) => element.id == value.id);
     listaJugador.removeAt(index);
   }
 
